8b4e1f8ec822d910c247c69f4c4e0086d3081fba
gremisanvaleria <gremisanvaleria@outlook.com>
2025-07-19 10:03:24 +0300

    Update raw_uc_mode.py

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -1,7 +1,7 @@
 from seleniumbase import SB
 
 with SB(uc=True, test=True) as sb:
-    url = "https://kick.com/onlyyysal"
+    url = "https://kick.com/brutalles"
     sb.uc_open_with_reconnect(url, 5)
     sb.uc_gui_click_captcha()
     sb.sleep(2)
